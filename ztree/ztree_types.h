5404d1583f7367f13983922eab1f1d9ce2170d80
jang1161 <jang020407@naver.com>
2026-05-04 00:51:02 +0900

    increase ilayer pool size

--- ztree/ztree_types.h
+++ ztree/ztree_types.h
@@ -31,9 +31,9 @@
 #define ZTREE_META_ZONE_0          0U
 #define ZTREE_META_ZONE_1          1U
 #define ZTREE_ILAYER_ZONE_START    2U   /* first zone of ILayer pool        */
-#define ZTREE_ILAYER_POOL_SIZE    16U   /* ILayer pool size (zones 2-17)    */
+#define ZTREE_ILAYER_POOL_SIZE   128U   /* ILayer pool size (zones 2-129)   */
 #define ZTREE_ILAYER_INIT_COUNT    3U   /* initial IZGroup size (paper: 4)  */
-#define ZTREE_LLAYER_ZONE_START   18U   /* first zone of LLayer pool        */
+#define ZTREE_LLAYER_ZONE_START  130U   /* first zone of LLayer pool        */
 #define ZTREE_LZGROUP_INIT_SIZE   10U   /* initial LZGroup total size       */
 #define ZTREE_LZGROUP_HOT_INIT     8U   /* initial hot  zones (paper: 6)    */
 #define ZTREE_LZGROUP_COLD_INIT    2U   /* initial cold zones (paper: 2)    */
